309826c5167bf52040225406fbf994d2e67a2c25
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-11-20 10:03:45 +0100

    change impl of instrumentation

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,10 +1,10 @@
 const pino = require("pino");
 
-const t = pino.transport({
-  target: "@baselime/pino-transport",
-  options: { baselimeApiKey: process.env.NEXT_PUBLIC_BASELIME_KEY as string },
-});
+// const t = pino.transport({
+//   target: "@baselime/pino-transport",
+//   options: { baselimeApiKey: process.env.NEXT_PUBLIC_BASELIME_KEY as string },
+// });
 
 export const logger = pino(
-  t
+  // t
 );
