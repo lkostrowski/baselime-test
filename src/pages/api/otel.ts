d20d20563c3c8d0ff7e54bd4c13d2c8dd4014ade
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-11-20 11:05:37 +0100

    wip

--- src/pages/api/otel.ts
+++ src/pages/api/otel.ts
@@ -5,22 +5,18 @@ import { startOtelSdk } from "../../lib/sdk";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await startOtelSdk();
 
-  console.log({
-    message: trace.getActiveSpan()?.spanContext(),
-  });
-
   const tracer = trace.getTracer("otel-test-route");
   const span = tracer.startSpan("test-route");
 
-  console.log({ message: "test console log" });
+  console.log("test console log");
 
-  console.log({
-    message: trace.getActiveSpan()?.spanContext(),
-  });
+  console.log(trace.getActiveSpan()?.spanContext());
 
   span?.setAttribute("query", JSON.stringify(req.query));
 
-  span?.addEvent("start handler");
+  span?.addEvent("start handler", {
+    foo: "Bar",
+  });
 
   span?.setStatus({ code: SpanStatusCode.OK });
 
